344a2122c4efebf3fd1e85231f9eba405af4ba86
Eric van Schaik <schaikejgv@gmail.com>
2023-02-17 15:09:37 +0100

    Made codeform larger

--- src/components/AddCode.vue
+++ src/components/AddCode.vue
@@ -1,5 +1,34 @@
 <template>
-  <v-btn @click="scroll">goto</v-btn>
+  <container>
+    <v-row justify="space-around">
+      <v-col md="2">
+      </v-col>
+      <v-col md="6">
+        <v-sheet class="d-flex justify-space-around" color="transparent">
+          <v-card class="pa-2 ma-2"
+            v-for="i in 5"
+            :key="i"
+          >
+          <div class="text-h5">
+            {{ i }}
+          </div>
+          </v-card>
+        </v-sheet>
+      </v-col>
+      <v-col md="2">
+        <v-sheet class="d-flex justify-space-around" color="transparent">
+          <v-card class="pa-2 my-2 mr-11">
+            <div class="text-h5">
+              0
+            </div>
+          </v-card>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-btn @click="scroll">Add Code</v-btn>
+    </v-row>
+  </container>
 </template>
 
 <script>
